b83d76041b0cbd0bb67cfecc2a7d9276b33a78e2
Mirris <>
2024-11-25 18:57:02 -0300

    animaciones mas efectos

--- Assets/bala.cs
+++ Assets/bala.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class bala : MonoBehaviour
 {
+    public GameObject sangre;
 
     [SerializeField] public int damage = 25;
 
@@ -14,6 +16,10 @@ public class bala : MonoBehaviour
         {
             collision.gameObject.GetComponent<enemigo>().ReceiveDamageFromBala(damage);
             Destroy(gameObject);
+
+            GameObject sangreClone = Instantiate (sangre, transform.position, transform.rotation);
+
+            Destroy(sangreClone, 0.2f);
         }
         Destroy(gameObject);
     }
